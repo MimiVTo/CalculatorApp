004320d93bb4703583cad88464df7307177ea02d
Mimi <mimivto2007@gmail.com>
2025-03-11 14:35:03 -0700

    design again

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -14,12 +14,12 @@ struct ContentView: View {
         ["7","8", "9", "x"],
         ["4","5", "6", "-"],
         ["1","2", "3", "+"],
-        ["0", " . " , "="]
+        ["0", "." , "="]
     ]
     
     //NUMBERS ------------
-    @State var num1: Double = 0
-    @State var num2: Double = 0
+    @State var num1: String = ""
+    @State var num2: String = ""
     @State var num1Set: Bool = false
     @State var num2Set: Bool = false
     
@@ -33,21 +33,13 @@ struct ContentView: View {
         VStack(alignment: .trailing){
             Spacer()
             Text("\(total)")
-                .font(.title)
+                .font(.largeTitle)
                 .foregroundColor(Color.white)
                 .padding()
             ForEach(numsAndOperations, id: \.self){ index in
                 HStack{
                     ForEach(index, id: \.self){ btn in
-                        if btn == "÷" || btn == "x" || btn == "-" || btn == "+" || btn == "="{
-                            OperationsView(name: "\(btn)")
-                        }
-                        else if btn == "AC" || btn == "+/-" || btn == "%"{
-                            ExtrasView(name: "\(btn)")
-                        }
-                        else{
-                            NumbersView(name: "\(btn)")
-                        }
+                        ButtonView(action: action, item: "\(btn)")
                     }
                 }
             }
@@ -59,82 +51,133 @@ struct ContentView: View {
                 .ignoresSafeArea()
         }
     }
+    
+    func action(){
+        
+    }
 }
 
 
 //STRUCTS TO DESIGN IT --------------------
-struct OperationsView: View{
-    var name: String = ""
-    var body: some View {
-        
-        //DESIGN
-        ZStack{
-            Circle()
-                .fill(Color.orange)
-            Button(action: setOperation, label: {
-                Text(name)
-                    .foregroundColor(Color.white)
-            })
-        }
-    }
-}
 
-struct ExtrasView: View{
-    var name: String = ""
+struct ButtonView: View {
+    var action: () -> Void
+    var item: String = ""
     var body: some View {
-        
-        //DESIGN
-        ZStack{
-            Circle()
-                .fill(Color(hex: "cccccc"))
-            Button(action: setExtra, label: {
-                Text(name)
-                    .foregroundColor(Color.black)
-            })
+        Button(action: action){
+            Text(item)
+                .font(.largeTitle)
+                .frame(width: decideWidthFrame(item: item), height: 80)
+                .background(backgroundColor(item: item))
+                .foregroundColor(fontColor(item: item))
+                .cornerRadius(50)
+        }
+    }
+    
+    func decideWidthFrame(item: String) -> CGFloat{
+        if item == "0"{
+            return 170
+        }
+        else{
+           return 80
+        }
+    }
+    func backgroundColor(item: String) -> Color{
+        if item == "=" || item == "+" || item == "-" || item == "x" || item == "÷"{
+            return .orange
+        }
+        else if item == "AC" || item == "+/-" || item == "%"{
+            return Color(hex: "cccccc")
+        }
+        else{
+            return .gray
+        }
+    }
+    func fontColor(item: String) -> Color{
+        if item == "AC" || item == "+/-" || item == "%"{
+            return .black
+        }
+        else {
+            return .white
         }
     }
 }
-
-struct NumbersView: View {
-    var name: String = ""
-    var body: some View {
-        
-        //DESIGN
-        ZStack{
-            if name == "0"{
-                Rectangle()
-                    .fill(Color.gray)
-                    .frame(width:180, height:80)
-                    .cornerRadius(90)
-                Button(action: setNumber, label: {
-                    Text(name)
-                        .foregroundColor(Color.white)
-                })
-            }
-            else{
-                Circle()
-                    .fill(Color.gray)
-                Button(action: setNumber, label: {
-                    Text(name)
-                        .foregroundColor(Color.white)
-                })
-            }
-        }
-    }
-}
+//struct ButtonsView: View{
+//    var name: String = ""
+//    var body: some View {
+//        
+//        //DESIGN FOR BUTTONS
+//        ZStack{
+//            if name == "="{
+//                Circle()
+//                    .fill(Color.orange)
+//                Button(action: equation, label: {
+//                    Text(name)
+//                        .foregroundColor(Color.white)
+//                })
+//            }
+//            else if name == "AC" || name == "+/-" || name == "%"{
+//                //DESIGN for EXTRAS
+//                Circle()
+//                    .fill(Color(hex: "cccccc"))
+//                Button(action: setExtra, label: {
+//                    Text(name)
+//                        .foregroundColor(Color.black)
+//                        .font(.title)
+//                })
+//            }
+//            else if name == "0"{
+//                Rectangle()
+//                    .fill(Color.gray)
+//                    .frame(width:180, height:80)
+//                    .cornerRadius(90)
+//                Button(action: setNumber, label: {
+//                    Text(name)
+//                        .foregroundColor(Color.white)
+//                        .font(.title)
+//                })
+//            }
+//            else if name == "1" || name == "2" || name == "3" || name == "4" || name == "5" || name == "6" || name == "7" || name == "8" || name == "9" || name == "."{
+//                Circle()
+//                    .fill(Color.gray)
+//                Button(action: setNumber, label: {
+//                    Text(name)
+//                        .foregroundColor(Color.white)
+//                        .font(.title)
+//                })
+//            }
+//            else{
+//                Circle()
+//                    .fill(Color.orange)
+//                Button(action: setOperation, label: {
+//                    Text(name)
+//                        .foregroundColor(Color.white)
+//                })
+//            }
+//        }
+//    }
+//}
 
 //FUNCTIONS ----------------
-
-func setNumber(){
+func handleClick(){
+    
+    
     
 }
-func setOperation(){
-    
-}
-func setExtra(){
-    
-}
-
+//
+//func setNumber(){
+//    
+//}
+//func setOperation(){
+//    
+//}
+//func setExtra(){
+//    
+//}
+//func equation(){
+//    
+//}
+//
 #Preview {
     ContentView()
 }
